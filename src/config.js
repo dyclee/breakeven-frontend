1bb1c47b6b673965a0c69ca2256613e17c69babe
David <dyclee@umich.edu>
2020-10-27 18:12:09 -0500

    added src

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const imageUrl = "http://localhost:8080";
+const baseUrl = `${imageUrl}/api`
+
+module.exports = {
+    imageUrl,
+    baseUrl
+}
---
+++ src/fetches/authentication.js
@@ -0,0 +1,16 @@
+const { response } = require('express');
+const { baseUrl } = require('../config');
+
+const getToken = async(email, password) => {
+    const res = await fetch(`${baseUrl}/session`, {
+        method: "put",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password}),
+    });
+
+    if (res.ok) {
+        const { token } = await response.json();
+        window.localStorage.setItem("token", token);
+        return token;
+    }
+}
