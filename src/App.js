4c230b20edbc800955e68204379530c9b536ee88
David <dyclee@umich.edu>
2020-10-28 15:12:36 -0500

    app with context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-
+import Context from './Context';
 import LoginPanel from './LoginPanel';
 
 const PrivateRoute = props => {
@@ -9,4 +9,14 @@ const PrivateRoute = props => {
             props.needLogin === true ? <Redirect to='/login' /> : props.children
         );
     }}/>);
+}
+
+const App = () => {
+    const { token, setToken } = useContext(Context);
+
+    useEffect(() => {
+        (async() => {
+            const localToken = window.localStorage.getItem('token');
+        })
+    })
 }
--- src/AppWithContext.js
+++ src/AppWithContext.js
@@ -1,4 +1,33 @@
-import React, { useStae, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Context from './Context';
-import App from './App;'
+import App from './App';
+
+const AppWithContext = (props) => {
+    const [token, setToken] = useState('');
+    const [formVisible, setFormVisible] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            const localToken = window.localStorage.getItem("token");
+            if (localToken) {
+                setToken(localToken);
+            }
+        })();
+    }, []);
+
+    const value = {
+        token,
+        setToken,
+        formVisible,
+        setFormVisible,
+    };
+
+    return (
+        <PokemonContext.Provider value={value}>
+            <App></App>
+        </PokemonContext.Provider>
+    )
+}
+
+export default AppWithContext
