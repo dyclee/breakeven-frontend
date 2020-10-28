c3d81f12acd3556866e2bd322abb81a2bdd64f7f
David <dyclee@umich.edu>
2020-10-28 12:26:08 -0500

    creating context

---
+++ src/AppWithContext.js
@@ -0,0 +1,4 @@
+import React, { useStae, useEffect } from 'react';
+
+import Context from './Context';
+import App from './App;'
--- src/LoginPanel.js
+++ src/LoginPanel.js
@@ -1,3 +1,10 @@
 import React, { useState, useContext} from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect,  } from 'react-router-dom';
 import { getToken } from './fetches/authentication';
+import Context from './Context';
+
+const LoginPanel = () => {
+    const [ email, setEmail ] = useState('demo@email.com');
+    const [ password, setPassword] = useState('passwordbro');
+    const { token, setToken } = useContext(Context);
+}
